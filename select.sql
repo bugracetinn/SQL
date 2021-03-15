a79da981cb40b8318d6db1c974ee9c3a83e5f678
bugracetinn <70979544+bugracetinn@users.noreply.github.com>
2021-03-15 15:15:33 +0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,19 +1,19 @@
-SELECT = seç 
-FROM = -den
-AS = olarak
-DISTINC = farklı 
-* = hepsini seçer
+-- SELECT = seç 
+-- FROM = -den
+-- AS = olarak
+--DISTINC = farklı 
+-- * = hepsini seçer
 
 SELECT * FROM tablo_adı 
 
-<------ örnek ---->  
+-- <------ örnek ---->  
 
--> SELECT * FROM customers 
+-- -> SELECT * FROM customers 
 
 customers tablasounda ki kayıtlı verilerin hepsini getir.
 
 
--> SELECT sütun_adı FROM tablo_adı
+-- -> SELECT sütun_adı FROM tablo_adı
 
 <------ örnek ------->
 
@@ -27,50 +27,50 @@ türkçe olarak aktarımı -> Oluşturulan tablodan istediğimiz sütün adları
  SELECT customerName,contactFirstName,contactLastName FROM `customers`
  türkçe olarak aktarımı -> customers tablosundaki customerName,contactFirstName,contactLastName sütün adlarının bilgierlini seçer ve gösterir. 
 
--> AS FROM KOMUTU
+-- -> AS FROM KOMUTU
 SELECT sütun_adı AS ‘tablo sütun adı’ FROM tablo_adı         * sütun_adı=veri tabanında tablo oluştururken verilen isim  *tablo sütun adı = en başta sütun adı koyduğumuz yeri hangi isim ile görmek istediğimiz kısım
                                                                  *tablo_adı = en başta veritabınını kuararken tabloya verdiğimiz isin
- <------ örnek -------->                                                                
+-- <------ örnek -------->                                                                
                                                                  
 SELECT customerName AS KULLANICIADLARI FROM customers
 türkçe olarak aktarımı -> customers veritabanından tablo adı customerName olan bilgileri seç (SELECT) tablo adını customerName yerine KULLANICIADLARI olarak(AS) göster.
 
--> SELECT DISTINC
+---> SELECT DISTINC
 SELECT DISTINCT sutun_adi FROM tablo_adi 
 türkçe olarak aktarımı -> sütün adındaki bilgieri aynı olanları gösterme sadece farklıları göster.
 
- <------ örnek ------>
+-- <------ örnek ------>
 SELECT DISTINCT postalCode FROM customers
-türkçe olarak aktarımı -> customers tablosundan postalCode sütunununda ki farklı girilmiş bilgileri seç 
+-- türkçe olarak aktarımı -> customers tablosundan postalCode sütunununda ki farklı girilmiş bilgileri seç 
 
 -> SELECT COUNT(*)
-COUNT(*) komutu ile kayıt sayısı listelenir.
+-- COUNT(*) komutu ile kayıt sayısı listelenir.
 SELECT COUNT(*) FROM tablo_adi
 
-<------- örnek ---------->
+-- <------- örnek ---------->
  
  SELECT COUNT(*) FROM customers
- türkçe olarak aktarımı -> customers tablosunda ki kayıtları seçer ve kaç tane kayıt olduğunu gösterir 
+-- türkçe olarak aktarımı -> customers tablosunda ki kayıtları seçer ve kaç tane kayıt olduğunu gösterir 
 
--> SELECT ...WHERE
-WHERE komutu filtreleme için kullanılır.
+-- -> SELECT ...WHERE
+-- WHERE komutu filtreleme için kullanılır.
 SELECT sütun_adi FROM tablo_adi WHERE sütun_adi= 'xxx'
 
-<------- örnek --------->
+-- <------- örnek --------->
 
 SELECT * FROM customers WHERE postalCode = '44000'
 türkçe olarak aktarımı -> posta kodu 44000 olan kullanıcıları customers tablosundan seçer ve gösterir.
 
-= yerine başka kullanma modelleri de vardır;
->,<,>=,<=,<>,BETWEEN,LIKE,IN gibi kullanımları da mevcuttur.
+--  = yerine başka kullanma modelleri de vardır;
+--  >,<,>=,<=,<>,BETWEEN,LIKE,IN gibi kullanımları da mevcuttur.
 
--> SELECT ... WHERE... AND/OR/NOT 
+-- -> SELECT ... WHERE... AND/OR/NOT 
 SELECT sütun_adi FROM tablo_adi WHERE sütun_adi= 'xxx' AND/OR/NOT sütun_adi='xxx'
 
 <-------- örnek --------->
 
 SELECT * FROM customers WHERE postalCode= '4400' AND credilimit > 100000
-türkçe olarak aktarımı -> credilimit sütunundan kredi limiti 1000.000'den büyük ve posta kodu 4400 olam costumers tablosunda ki kullanıcıları seçer ve gösterir.
+-- türkçe olarak aktarımı -> credilimit sütunundan kredi limiti 1000.000'den büyük ve posta kodu 4400 olam costumers tablosunda ki kullanıcıları seçer ve gösterir.
 
 
 
