0378bec9505a4c0c7a6a1b55c95dd692bab7ed9b
bugracetinn <70979544+bugracetinn@users.noreply.github.com>
2021-03-15 17:25:55 +0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,4 +1,6 @@
+-- INSERT -> Ekle INTO -> İçime yani içine ekle anlamına gelir.
 
-
-
-INSERT INTO orders VALUES(
+INSERT INTO tablo_adi VALUES(deger_1,deger_2,deger_3...) -- Şeklinde yazılır.
+-- <--- örnek --->
+INSERT INTO orders 
+VALUES(10426,'2021-03-11','2121-03-13','NULL','In Process','',103) --> İstenen bilgiler sırasıyla yazılır ve eklenir.
