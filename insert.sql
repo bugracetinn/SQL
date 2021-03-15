4774d013a75116068d3a57657b7394a95985c337
bugracetinn <70979544+bugracetinn@users.noreply.github.com>
2021-03-15 17:45:37 +0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -2,5 +2,11 @@
 
 INSERT INTO tablo_adi VALUES(deger_1,deger_2,deger_3...) -- Şeklinde yazılır.
 -- <--- örnek --->
+--sipariş ekleme
 INSERT INTO orders 
 VALUES(10426,'2021-03-11','2121-03-13','NULL','In Process','',103) --> İstenen bilgiler sırasıyla yazılır ve eklenir.
+ --yada
+ INSERT INTO orders
+ (orderNumber,  orderDate,requiredDate,status,customerNumber)
+ VALUES
+ (
