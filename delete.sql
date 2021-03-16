33a620d1316f37ba0ed698913712ef044021d4db
bugracetinn <70979544+bugracetinn@users.noreply.github.com>
2021-03-16 17:16:31 +0300

    Create delete.sql

---
+++ delete.sql
@@ -0,0 +1,9 @@
+-- DELETE = sil
+
+DELETE FROM tablo_adi 
+WHERE sütun_adi = 'silincek sütun değeri'
+Şeklinde sorgu yazılabilir.
+
+-- <--- örnek --->
+DELETE FROM orders 
+WHERE orderNumber = 10426
