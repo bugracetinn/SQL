a8d6fc363c67301e49d3fece87a11872a4a72159
bugracetinn <70979544+bugracetinn@users.noreply.github.com>
2021-03-16 17:01:02 +0300

    Create uptade.sql

---
+++ uptade.sql
@@ -0,0 +1,19 @@
+-- UPDATE = Güncelle
+
+UPDATE tablo_adi
+SET sütun_adi='istediğiniz güncelleme',sütun_adi='istediğiniz güncelleme' -- -> Bu kısımda güncelleceğiniz bilgiler girilir. 
+WHERE sütun_adi='güncellemek istediğiniz değere sahip satır' -- -> Bu kısımda hangi bilgiye sahip satırındaki bilgileri güncelleceğiniz bilgi yazalır bir nevi filtreleme yapılır.
+şeklinde yazılabilir.
+
+-- <--- örnek ---->
+
+UPDATE orders
+SET shippedDate = '2021-03-11',status='Shipped',comments='yazmak istediğiniz yorum'
+WHERE orderNumber = 10426
+-- <---- örnek --->
+
+UPDATE customers
+--veritabanında satış danışmanının satış yaptığı kodunu güncellemek bu sorgu ile yapılır.
+
+SET salesRepEmloyeeNumber=1401
+WHERE salesRepEmloyeeNumber=14611 
